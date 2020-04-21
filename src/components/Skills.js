b68ad52e88b3a7d6c3cec45159ae508fbd1dd02f
David Rodríguez <devdrod@gmail.com>
2020-04-21 11:27:42 -0600

    Landingandskills

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -8,25 +8,43 @@ export class Skills extends Component {
     render() {
         return (
            
-    <div className="flexbox-container body">
-<div className="skills-item">
+<div className="flexbox-container">
+<div className="flexbox-skill skills-item">
 
-<h1> Tools and Languages </h1>
+<h1 style={{textAlign:'center'}}> Tools &#38; Languages </h1>
 <br/>
-<ul>
+
+
+<div className='row'> 
+
+<div className="column">
+
 <li>HTML5</li>
 <li>CSS</li>
 <li>SAS</li>
-<li>Bootstrap-4</li>
+<li>Bootstrap4</li>
+</div>
+
+<div className="column">
+
+
 <li>JavaScript</li>
 <li>React.js</li>
 <li>Gatsby.js</li>
+
+
+</div>
+
+<div className="column" style={{borderStyle:"none"}}>
+
 <li>Mongodb</li>
 <li>Express.js</li>
 <li>Node.js</li>
-</ul>
-    </div>
-    </div>
+</div>
+
+</div>
+</div>
+</div>
   )
 }
 }
